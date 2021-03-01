107bdae3803a751c218e83a4d894a87056f7acd2
Akseli Palen <akseli.palen@gmail.com>
2021-03-01 16:41:38 +0200

    Lint all and repair index.js lint issues

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 
-var gv = require('./lib/genversion');
-var v = require('./lib/version');
-var cv = require('./lib/checkVersion');
+const gv = require('./lib/genversion')
+const v = require('./lib/version')
+const cv = require('./lib/checkVersion')
 
-exports.check = gv.check;
-exports.generate = gv.generate;
-exports.version = v;
-exports.checkVersion = cv;
+exports.check = gv.check
+exports.generate = gv.generate
+exports.version = v
+exports.checkVersion = cv
