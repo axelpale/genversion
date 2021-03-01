af807d89fe036df9b93da9c17acbafbcb503c7c3
Akseli Palen <akseli.palen@gmail.com>
2021-03-01 23:07:26 +0200

    Remove separate checkversion exports

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 
 const gv = require('./lib/genversion')
 const v = require('./lib/version')
-const cv = require('./lib/checkVersion')
 
 exports.check = gv.check
 exports.generate = gv.generate
 exports.version = v
-exports.checkVersion = cv
